92096f5a8e8f3b392011910702959c25164f7f38
ashtyn3 <ashtynmorel@gmail.com>
2025-05-08 20:20:30 -0400

    remove uneeded stuff

--- src/memory.zig
+++ src/memory.zig
@@ -30,10 +30,6 @@ clock: *Clock,
 
 contigous: [constants.shared_mem_len]u8,
 
-read_recieving_bus: *Bus(ReadRecieve, constants.bus_max),
-read_sending_bus: *Bus(ReadSend, constants.bus_max),
-
-write_recieving_bus: *Bus(WriteRecieve, constants.bus_max),
 allocator: std.mem.Allocator,
 
 const Self = @This();
@@ -43,29 +39,9 @@ pub fn init(a: std.mem.Allocator, clock: *Clock) !*Self {
     s.* = .{
         .clock = clock,
         .contigous = std.mem.zeroes([constants.shared_mem_len]u8),
-        .read_recieving_bus = try .init(a),
-        .read_sending_bus = try .init(a),
-        .write_recieving_bus = try .init(a),
         .allocator = a,
     };
     return s;
-}
-
-pub fn destroy(self: *Self) void {
-    self.allocator.destroy(self.read_recieving_bus);
-    self.allocator.destroy(self.read_sending_bus);
-    self.allocator.destroy(self.write_recieving_bus);
-}
-
-pub fn recieve_writes(self: *Self) void {
-    for (self.write_recieving_bus.sink()) |recv| {
-        if (recv.data.len != 0) {
-            // assert(recv.data.len >= 32);
-            const width = recv.address + recv.data.len;
-            assert(width <= self.contigous.len);
-            @memcpy(self.contigous[recv.address..width], recv.data[0..recv.data.len]);
-        }
-    }
 }
 
 pub fn send_write(self: *Self, recv: WriteRecieve) void {
@@ -91,23 +67,6 @@ pub fn real_read(self: *Self, r: ReadRecieve) ReadSend {
         .len = r.len,
         .data = self.contigous[r.address..(r.address + r.len)],
     };
-}
-pub fn complete_reads(self: *Self) void {
-    if (self.read_recieving_bus.active != 0) {
-        const data = self.read_recieving_bus.sink();
-        for (data) |recv| {
-            if (recv.len != 0) {
-                self.read_sending_bus.put(.{
-                    .start = recv.address,
-                    .pc = recv.pc,
-                    .thread_id = recv.thread_id,
-                    .cluster_id = recv.cluster_id,
-                    .len = recv.len,
-                    .data = self.contigous[recv.address..(recv.address + recv.len)],
-                });
-            }
-        }
-    }
 }
 pub fn debug(self: *Self) void {
     var stdout = std.io.getStdOut().writer();
